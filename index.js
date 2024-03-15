24b6e8548c1f47c315bcc421ada520e6168b346e
mariansurdu112 <surdu_marian94@yahoo.com>
2024-03-16 01:09:28 +0200

    'add_array_support'

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const areObjects = require('are-objects');
 const areArrays = require('are-arrays');
-const sum = (a, b) => a + b
+const sum = (a, b) => {
+    if(a instanceof Array && b instanceof Array){
+        const sumA = a.reduce((partialSum, x) => partialSum + x, 0);
+        const sumB = b.reduce((partialSum, x) => partialSum + x, 0);
+        return sumA+sumB;
+    } else{
+        return a+b;
+    }
+}
 exports.sum = sum
